4294b661e0f9cbcd690d062141780741c72e438b
Sohrab Salehi <sohrab.salehi@gmail.com>
2014-01-15 11:58:30 -0800

    * Added the simulation part of the tutorial.

--- scripts/jan15-tut2-playwithprobablity.R
+++ scripts/jan15-tut2-playwithprobablity.R
@@ -1,4 +1,5 @@
 # second lab
+# basic plotting, gamma distribution
 
 min.y = 0
 max.y = 20
@@ -20,3 +21,33 @@ f <- function(col, ...) {
 }
 
 plot.status <- mapply(f, shape=shapes, scale=scales, col=cols)
+
+
+# Simulating Data
+set.seed(1)
+
+gamma.shape = 10
+gamma.scale = 0.1
+
+rgamma(n = 5, shape=gamma.shape, scale=gamma.scale)
+
+num.of.samples = 1000
+y = rgamma(n = num.of.samples, shape=gamma.shape, scale=gamma.scale)
+
+# put it in a density and a histogram
+estimated.density = density(y)
+estimated.hist = hist(y)
+
+# plot themrug(y, col = "orange")
+plot(estimated.density)
+plot(estimated.hist)
+
+# cool command, adds the data points
+rug(y, col = "orange")
+
+x <- seq(0, 3, length = 1000)
+true.density <- dgamma(x, shape = gamma.shape, scale = gamma.scale)
+
+# This plots the true density lines
+lines(x, true.density, col = "red")
+
